03e943c92b2ccb72e2dff84176233d39a8866308
progbagger <thrixstudio@yandex.ru>
2022-03-23 14:36:35 +0700

    develop - Adapted onto Kirill`s changes

--- Makefile
+++
@@ -1,7 +0,0 @@
-GCC_F=-Wall -Werror -Wextra
-NAME=-o Alias
-
-all: build
-
-build: main.c ./include/main.h
-	gcc $(GCC_F) main.c $(NAME)
---
+++ src/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_ALIAS_LIB_H_
+#define MAIN_ALIAS_LIB_H_
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_NUM_COM 4
+
+#endif  // MAIN_ALIAS_LIB_H_
